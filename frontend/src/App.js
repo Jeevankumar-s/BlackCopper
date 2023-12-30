37307e69a6ce79e6997dc8f47cd213a68fa52efa
jeevankumar-s <jeevenkumar2003@gmail.com>
2023-12-31 00:46:15 +0530

    final

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3303/api/data');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        console.log(result)
+        setData(result);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+      <ul>
+        {data.map((item) => (
+          <li key={item._id}>
+          <p>{item.title}</p>
+          <p>{item.sector}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
